ac9f5502eff2eec094190b952a6880c9a6bb5e24
soichiro yoshida <choruscola@gmail.com>
2017-10-28 18:39:57 +0000

    post

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'WelcomeController@index');
 
 Route::get('signup', 'Auth\AuthController@getRegister')->name('signup.get');
 Route::post('signup', 'Auth\AuthController@postRegister')->name('signup.post');
@@ -21,6 +19,7 @@ Route::post('signup', 'Auth\AuthController@postRegister')->name('signup.post');
 Route::get('login', 'Auth\AuthController@getLogin')->name('login.get');
 Route::post('login', 'Auth\AuthController@postLogin')->name('login.post');
 Route::get('logout', 'Auth\AuthController@getLogout')->name('logout.get');
-Route::group(['middlewere' => 'Auth'], function() {
+Route::group(['middlewere' => 'auth'], function() {
     Route::resource('users','usesController',['only' =>['index', 'show']]);
+    Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destoroy']]);
 });
